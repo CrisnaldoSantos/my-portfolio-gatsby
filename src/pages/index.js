f75b2b0f27a4bb3691a383259702b4687d5efb15
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2020-03-26 05:22:05 -0300

    adição componente SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,14 @@ import Header from "../components/header";
 import About from "../components/about";
 import Jobs from "../components/jobs";
 import Container from '../components/container';
+import SEO from '../components/seo';
 
 export default ({ data }) =>
+<>
+    <SEO title=" " />
     <Container>
       <Header/>
       <About/>
       <Jobs/>
     </Container>
-
+</>
