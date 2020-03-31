361eaa57ae79280520f1af8d5fd41be35a3fbe40
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2020-03-30 23:46:11 -0300

    inclusão do required nos campos do form contatos

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,13 +9,13 @@ export default ()=>
     <div className="flex justify-center">
         <form name="contact" action="https://formspree.io/xlelqdpr" method="POST" className="w-full sm:w-full md:w-1/2 lg:w-1/2   px-1 pt-6 pb-8 mb-4">
             <div className="mb-2">
-                <input placeholder="Nome" className="text-xl shadow appearance-none border border-black rounded-lg w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" />
+                <input placeholder="Nome" className="text-xl shadow appearance-none border border-black rounded-lg w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" required/>
             </div>
             <div className="mb-2">
-                <input placeholder="Email" type="email" name="_replyto" className="text-xl border-black shadow appearance-none border rounded-lg w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"/>
+                <input placeholder="Email" type="email" name="_replyto" className="text-xl border-black shadow appearance-none border rounded-lg w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" required/>
             </div>
             <div className="mb-2"> 
-                <textarea placeholder="Mensagem" className="text-xl border-black shadow appearance-none border rounded-lg w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" type="text" name="message"></textarea>
+                <textarea placeholder="Mensagem" className="text-xl border-black shadow appearance-none border rounded-lg w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" type="text" name="message" required></textarea>
             </div>
             <div className="flex justify-center">
                 <button className="btn btn-xl bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-lg block focus:outline-none focus:shadow-outline" type="submit">Enviar</button>
